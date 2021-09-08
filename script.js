e3312fa292bae1048b14061192f6d2dd13db67d0
Sands-45 <sandsqa@hotmail.com>
2021-09-08 21:24:03 +0200

    Update

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const ul = document.getElementById("fetchedData");
+const addBtn = document.getElementById("addData");
+const url =
+  "https://script.google.com/macros/s/AKfycbw77BhNZceqxTbOtllVIiZUhFHoQKr33vKeUKQYxi9fvNEwxco/exec";
 
+//Fetch API Json Data
 fetch("data.json")
   .then((response) => {
     return response.json();
@@ -16,16 +20,32 @@ fetch("data.json")
     console.log(error);
   });
 
-//  Using async and await
+//fetch API Using async and await
 const getData = async () => {
-  const response = await fetch("data.json");
+  const response = await fetch(url);
   const data = await response.json();
   return data;
 };
 getData()
   .then((data) => {
-    console.log(data[0]);
+    console.log(data[0].hi);
   })
   .catch((error) => {
     console.log("Data not Found", error);
   });
+
+//Fetch API Posting Data
+function sheetData(){
+	fetch(url,{
+		method:"POST",
+		mode:"no-cors",
+		cache:"no-cache",
+		headers:{
+			"Content-type":"application/json"
+		},
+		redirect:"follow",
+		body: JSON.stringify({restuarant:"Oliven"})
+	});
+}
+
+addBtn.addEventListener("click",sheetData);
